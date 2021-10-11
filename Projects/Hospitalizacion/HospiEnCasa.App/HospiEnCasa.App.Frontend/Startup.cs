428dd3b45ce66690d173791131bf93d315f9cffd
Gustavo Romero <gasifero@gmail.com>
2021-10-11 15:03:39 -0500

    hospi-front1 - before launch Pacientes view

--- Projects/Hospitalizacion/HospiEnCasa.App/HospiEnCasa.App.Frontend/Startup.cs
+++ Projects/Hospitalizacion/HospiEnCasa.App/HospiEnCasa.App.Frontend/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using HospiEnCasa.App.Persistencia.AppRepositorios;
 
 namespace HospiEnCasa.App.Frontend
 {
@@ -24,6 +25,7 @@ namespace HospiEnCasa.App.Frontend
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            /* services.AddSingleton<IRepositorioPaciente, RepositorioPaciente>(); */
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
