312e7dc49a2b5ca73880473c965288b19dae220a
Gustavo Romero <gasifero@gmail.com>
2021-10-15 13:35:32 -0500

    hospi-front1-fam-> list familiar from paciente detail view

--- Projects/Hospitalizacion/HospiEnCasa.App/HospiEnCasa.App.Frontend/Pages/Pacientes/Detail.cshtml.cs
+++ Projects/Hospitalizacion/HospiEnCasa.App/HospiEnCasa.App.Frontend/Pages/Pacientes/Detail.cshtml.cs
@@ -13,11 +13,12 @@ namespace HospiEnCasa.App.Frontend.Pages
     {
         private readonly IRepositorioPaciente _repositorioPaciente;
         public Paciente Paciente { get; set; }
+        public FamiliarDesignado FamiliarDesignado { get; set; }
         public DetailModel()
         {
             this._repositorioPaciente = new RepositorioPaciente(new HospiEnCasa.App.Persistencia.AppContext());
         }
-        public IActionResult OnGet(int idPaciente)
+        /* public IActionResult OnGet(int idPaciente)
         {
             Paciente = _repositorioPaciente.GetPaciente(idPaciente);
             if (Paciente == null)
@@ -26,6 +27,35 @@ namespace HospiEnCasa.App.Frontend.Pages
             }
             else
                 return Page();
+        } */
+
+        public IActionResult OnGet(int idPaciente)
+        {
+            Paciente = _repositorioPaciente.GetPaciente(idPaciente);
+            if (Paciente == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+            else
+                FamiliarDesignado = _repositorioPaciente.GetFamiliarDesignado(idPaciente);
+                return Page();
         }
+
+        /* public void OnGetFamiliar(int? idPaciente)
+        {
+            if (idPaciente.HasValue)
+            {
+                Paciente = _repositorioPaciente.GetPaciente(idPaciente.Value);
+            }
+
+            if (Paciente == null)
+            {
+                RedirectToPage("./NotFound");
+            }
+            else
+            {
+                FamiliarDesignado = _repositorioPaciente.GetFamiliarDesignado(idPaciente.Value);
+            }
+        } */
     }
 }
