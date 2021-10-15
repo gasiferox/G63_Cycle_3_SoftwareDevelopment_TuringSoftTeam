fc970fb1142d83def766f4261d6284c6e4eee9f2
Gustavo Romero <gasifero@gmail.com>
2021-10-15 12:56:10 -0500

    hospi-front1-> signos add console update

---
+++ Projects/Hospitalizacion/HospiEnCasa.App/HospiEnCasa.App.Persistencia/Migrations/20211014210010_InitFront1.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HospiEnCasa.App.Persistencia.Migrations
+{
+    public partial class InitFront1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Telefono",
+                table: "Personas",
+                type: "varchar(10)",
+                maxLength: 10,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Personas",
+                type: "varchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Documento",
+                table: "Personas",
+                type: "varchar(15)",
+                maxLength: 15,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Apellido",
+                table: "Personas",
+                type: "varchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Foto",
+                table: "Personas",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Foto",
+                table: "Personas");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Telefono",
+                table: "Personas",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(10)",
+                oldMaxLength: 10,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Personas",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Documento",
+                table: "Personas",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(15)",
+                oldMaxLength: 15);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Apellido",
+                table: "Personas",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldMaxLength: 50);
+        }
+    }
+}
